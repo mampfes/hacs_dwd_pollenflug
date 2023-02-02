3445b02266874d8bef9a17a1961f36b4e7edfa8b
mampfes <mampfes@users.noreply.github.com>
2023-02-02 19:32:50 +0100

    use async_forward_entry_setups instead of async_setup_platforms
    fix #11

--- custom_components/dwd_pollenflug/__init__.py
+++ custom_components/dwd_pollenflug/__init__.py
@@ -33,7 +33,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     # add pollen region to shell
     shell.add_entry(entry)
 
-    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     return True
 
